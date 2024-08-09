df08c974d084a871bc878500b478313961ad2b73
ks6088ts <ks6088ts@gmail.com>
2024-08-09 11:49:43 +0900

    additional fix for step2

--- exercisefiles/springboot/copilot-demo/src/main/java/com/microsoft/hackathon/copilotdemo/controller/DemoController.java
+++ exercisefiles/springboot/copilot-demo/src/main/java/com/microsoft/hackathon/copilotdemo/controller/DemoController.java
@@ -1,5 +1,8 @@
 package com.microsoft.hackathon.copilotdemo.controller;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
